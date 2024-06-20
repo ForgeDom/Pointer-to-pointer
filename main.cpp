a267d8a5b13068b8b774eea4d0507c1bf812a1a5
ForgeDom <maxstill2006@gmail.com>
2024-06-20 20:07:04 +0300

    +

--- main.cpp
+++ main.cpp
@@ -3,74 +3,70 @@
 using namespace std;
 
 int** createArray(int rows, int cols) {
-    int** array = new int* [rows];
-    for (int i = 0; i < rows; ++i) {
-        array[i] = new int[cols];
-        for (int j = 0; j < cols; ++j) {
-            array[i][j] = rand() % 10;
-        }
-    }
-    return array;
+	int** array = new int* [rows];
+	for (int i = 0; i < rows; ++i) {
+		array[i] = new int[cols];
+		for (int j = 0; j < cols; ++j) {
+			array[i][j] = rand() % 10;
+		}
+	}
+	return array;
 }
 
-void addColumn(int**& array, int rows, int& cols, int* newCol, int position) {
-    if (position < 0 || position > cols) {
-        cout << "Wrong line" << endl;
-        return;
-    }
+void removeColumn(int**& array, int rows, int& cols, int position) {
+	if (position < 0 || position > cols) {
+		cout << "Wrong line" << endl;
+		return;
+	}
 
-    for (int i = 0; i < rows; ++i) {
-        int* newRow = new int[cols + 1];
-        for (int j = 0, k = 0; j <= cols; ++j) {
-            if (j == position) {
-                newRow[j] = newCol[i];
-            }
-            else {
-                newRow[j] = array[i][k++];
-            }
-        }
-        delete[] array[i];
-        array[i] = newRow;
-    }
-    cols++;
+	for (int i = 0; i < rows; ++i) {
+		int* newRow = new int[cols - 1];
+		for (int j = 0, k = 0; j < cols; ++j) {
+			if (j != position) {
+				newRow[k++] = array[i][j];
+			}
+		}
+		delete[] array[i];
+		array[i] = newRow;
+	}
+	cols--;
 }
 
 void freeArray(int** array, int rows) {
-    for (int i = 0; i < rows; ++i) {
-        delete[] array[i];
-    }
-    delete[] array;
+	for (int i = 0; i < rows; ++i) {
+		delete[] array[i];
+	}
+	delete[] array;
 }
 
 void printArray(int** array, int rows, int cols) {
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            cout << array[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			cout << array[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
 }
 
 int main() {
-    srand(time(0));
+	srand(time(0));
 
-    int rows = 5;
-    int cols = 3;
+	int rows = 3;
+	int cols = 8;
 
-    int** array = createArray(rows, cols);
+	int** array = createArray(rows, cols);
 
-    printArray(array, rows, cols);
+	printArray(array, rows, cols);
 
-    int newCol[] = { 7, 8, 9, 3, 2 };
-    int position;
-    cout << "Enter column: ";
-    cin >> position;
+	int position;
+	cout << "Enter column: ";
+	cin >> position;
 
-    addColumn(array, rows, cols, newCol, position);
-    printArray(array, rows, cols);
+	removeColumn(array, rows, cols, position);
+	printArray(array, rows, cols);
 
-    freeArray(array, rows);
+	freeArray(array, rows);
 
-    return 0;
+	return 0;
 }
