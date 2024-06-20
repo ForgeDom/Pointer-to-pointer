5a555d09fc06b7831fad0e6a9674e088f8e9ae9e
ForgeDom <maxstill2006@gmail.com>
2024-06-20 19:56:33 +0300

    +

--- main.cpp
+++ main.cpp
@@ -2,7 +2,75 @@
 #include <ctime>
 using namespace std;
 
-int main() {
+int** createArray(int rows, int cols) {
+    int** array = new int* [rows];
+    for (int i = 0; i < rows; ++i) {
+        array[i] = new int[cols];
+        for (int j = 0; j < cols; ++j) {
+            array[i][j] = rand() % 10;
+        }
+    }
+    return array;
+}
 
-	return 0;
+void addColumn(int**& array, int rows, int& cols, int* newCol, int position) {
+    if (position < 0 || position > cols) {
+        cout << "Wrong line" << endl;
+        return;
+    }
+
+    for (int i = 0; i < rows; ++i) {
+        int* newRow = new int[cols + 1];
+        for (int j = 0, k = 0; j <= cols; ++j) {
+            if (j == position) {
+                newRow[j] = newCol[i];
+            }
+            else {
+                newRow[j] = array[i][k++];
+            }
+        }
+        delete[] array[i];
+        array[i] = newRow;
+    }
+    cols++;
+}
+
+void freeArray(int** array, int rows) {
+    for (int i = 0; i < rows; ++i) {
+        delete[] array[i];
+    }
+    delete[] array;
+}
+
+void printArray(int** array, int rows, int cols) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int main() {
+    srand(time(0));
+
+    int rows = 5;
+    int cols = 3;
+
+    int** array = createArray(rows, cols);
+
+    printArray(array, rows, cols);
+
+    int newCol[] = { 7, 8, 9, 3, 2 };
+    int position;
+    cout << "Enter column: ";
+    cin >> position;
+
+    addColumn(array, rows, cols, newCol, position);
+    printArray(array, rows, cols);
+
+    freeArray(array, rows);
+
+    return 0;
 }
