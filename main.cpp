b7bd3861f3eee9ec40bb960223776ca6007ec2f4
ForgeDom <maxstill2006@gmail.com>
2024-06-20 20:02:50 +0300

    +

--- main.cpp
+++ main.cpp
@@ -13,26 +13,23 @@ int** createArray(int rows, int cols) {
     return array;
 }
 
-void addColumn(int**& array, int rows, int& cols, int* newCol, int position) {
+void removeColumn(int**& array, int rows, int& cols, int position) {
     if (position < 0 || position > cols) {
         cout << "Wrong line" << endl;
         return;
     }
 
     for (int i = 0; i < rows; ++i) {
-        int* newRow = new int[cols + 1];
-        for (int j = 0, k = 0; j <= cols; ++j) {
-            if (j == position) {
-                newRow[j] = newCol[i];
-            }
-            else {
-                newRow[j] = array[i][k++];
+        int* newRow = new int[cols - 1];
+        for (int j = 0, k = 0; j < cols; ++j) {
+            if (j != position) {
+                newRow[k++] = array[i][j];
             }
         }
         delete[] array[i];
         array[i] = newRow;
     }
-    cols++;
+    cols--;
 }
 
 void freeArray(int** array, int rows) {
@@ -55,19 +52,18 @@ void printArray(int** array, int rows, int cols) {
 int main() {
     srand(time(0));
 
-    int rows = 5;
-    int cols = 3;
+    int rows = 3;
+    int cols = 8;
 
     int** array = createArray(rows, cols);
 
     printArray(array, rows, cols);
 
-    int newCol[] = { 7, 8, 9, 3, 2 };
     int position;
     cout << "Enter column: ";
     cin >> position;
 
-    addColumn(array, rows, cols, newCol, position);
+    removeColumn(array, rows, cols, position);
     printArray(array, rows, cols);
 
     freeArray(array, rows);
